88c91920b113dbad1cd4f7500d598e78febf6c73
hsj4436 <hsj4436@naver.com>
2022-07-17 03:28:22 +0900

    festival list first commit / main page navigation 연결, festival 이미지 주소 연결 안함

--- category/myApp/views.py
+++ category/myApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.db import connection
+from django.core.paginator import Paginator
+from .models import Festival
+from .models import FestivalInfo
+from .models import FestivalImg
 
 # Create your views here.
 def showTable(request):
@@ -16,3 +20,22 @@ def showTable(request):
             hotspot_list.append({'Name' : row[0], 'Address' : row[1], 'Category' : row[2], 'Search' : row[3]})
 
     return render(request, 'myApp\index.html', {"hotspot" : hotspot_list})
+
+
+def show_festival(request):
+    festivals = Festival.objects.all().values().order_by('festival_id')
+
+    festival_pg = Paginator(festivals, 10)
+    page = int(request.GET.get('page', 1))
+    festival_list = festival_pg.get_page(page)
+
+    return render(request, 'myApp/festival.html', {'festival_list': festival_list})
+
+
+def festival_detail(request, festival_id):
+    festival = Festival.objects.get(festival_id=festival_id)
+    festival_info = FestivalInfo.objects.get(festival_id=festival_id)
+    festival_img = FestivalImg.objects.filter(festival_id=festival_id)
+
+    return render(request, 'myApp/festival_detail.html', {'festival': festival, 'festival_info': festival_info,
+                                                          'festival_img': festival_img})
